b067b476a4e40a0f8354e97513050992e64ef6f4
DESKTOP-PV6QE6K\user <jurijs.miscenkovs@gmail.com>
2021-07-19 13:24:39 +0300

    Set only image animation

--- app/src/main/java/com/example/nasajm/ui/pages/earth/EarthFragment.kt
+++ app/src/main/java/com/example/nasajm/ui/pages/earth/EarthFragment.kt
@@ -3,11 +3,17 @@ package com.example.nasajm.ui.pages.earth
 import android.app.Application
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.transition.ChangeBounds
+import androidx.transition.ChangeImageTransform
+import androidx.transition.TransitionManager
+import androidx.transition.TransitionSet
 import com.bumptech.glide.Glide
 import com.example.nasajm.R
 import com.example.nasajm.databinding.FragmentEarthBinding
@@ -20,6 +26,8 @@ class EarthFragment : Fragment(R.layout.fragment_earth) {
     private val viewBinding: FragmentEarthBinding by viewBinding(
         FragmentEarthBinding::bind
     )
+
+    var isClicked = false
 
     private val viewModel: EarthViewModel by viewModels {
         EarthFragmentFactory(
@@ -43,6 +51,22 @@ class EarthFragment : Fragment(R.layout.fragment_earth) {
                 Glide.with(viewBinding.earthImage)
                     .load(it)
                     .into(viewBinding.earthImage)
+            }
+        }
+
+        viewBinding.earthImage.setOnClickListener {
+            isClicked = !isClicked
+            TransitionManager.beginDelayedTransition(
+                viewBinding.earthFragment, TransitionSet()
+                    .addTransition(ChangeBounds())
+                    .addTransition(ChangeImageTransform())
+            )
+            val params: ViewGroup.LayoutParams = viewBinding.earthImage.layoutParams
+            //params.height = if (isClicked) ViewGroup.LayoutParams.MATCH_PARENT else ViewGroup.LayoutParams.WRAP_CONTENT
+            viewBinding.earthImage.apply {
+                layoutParams = params
+                scaleType =
+                    if (isClicked) ImageView.ScaleType.CENTER_CROP else ImageView.ScaleType.FIT_CENTER
             }
         }
     }
